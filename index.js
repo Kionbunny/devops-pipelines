072ae9908453a7edf08c181e88295ac8425f94a9
M Ajay Kumar <majay.kumar@hcltech.com>
2026-05-12 15:01:07 +0530

    Testing CI-Pipe line

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log('Hello, World!');
 console.log('Im learning DevOps');
 console.log('This is a simple Node.js application');
+console.log('I am excited to learn more about DevOps pipelines')
